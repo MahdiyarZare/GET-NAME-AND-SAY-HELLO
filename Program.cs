9438daf6908b30b9675dfdc5ac15e38b2b7f9474
MahdiyarZare <mahdiyarzare@yahoo.com>
2023-05-26 23:50:42 +0430

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GET_NAME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+        }
+    }
+}
